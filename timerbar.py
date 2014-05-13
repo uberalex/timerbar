5a00baa3a5f3895571aea708b14ebea98d24805b
Alex O'Connor <oconnoat@gmail.com>
2014-05-13 14:03:58 +0100

    completely rewrote timing to use datetime, timedelta and so on. Now integrates parsedatetime for natural language custom input

--- timerbar.py
+++ timerbar.py
@@ -7,93 +7,100 @@
 import os
 import sys
 import rumps # uses the rumps library from http://github.com/tito/rumps
-import time
+import parsedatetime # natural language expressions for parsing https://github.com/bear/parsedatetime
+from datetime import datetime, timedelta # temporal objects
 
 __author__ = "Alexander O'Connor <oconnoat@gmail.com>"
-__copyright__ = "Copyright 2012, Alexander O'Connor <oconnoat@gmail.com>"
+__copyright__ = "Copyright 2014, Alexander O'Connor <oconnoat@gmail.com>"
 __credits__ = ["Alexander O'Connor <oconnoat@gmail.com>"]
 __license__ = "Copyright"
-__version__ = "1.4"
+__version__ = "1.5"
 __email__ = "Alexander O'Connor <oconnoat@gmail.com>"
 __status__ = "Release"
 
-timeleft = 0
-elapsed = 0
+target_time = None
 notify = False
-start_time = 0
-
 
 @rumps.timer(1)
 def tick(sender):
     """ Called automatically every second """
-    global elapsed
-    global start_time
-    global timeleft
+    global target_time
     global notify
     global app
 
-    elapsed = time.time() - start_time
+    if target_time:
+        timeleft = target_time - datetime.now()
 
-    if timeleft > elapsed :
-        app.title = "(%02d:%02d)" %  ((timeleft - elapsed) / 60, (timeleft - elapsed) % 60)
+        app.title = "(%02d:%02d:%02d)" % (timeleft.total_seconds()//3600,(timeleft.seconds % 3600)//60, timeleft.seconds%60)
 
-    if (timeleft - elapsed) < 1 and notify:
-        app.title = "Done!"
-        rumps.notification(title="Countdown Done!", subtitle="TimerBar", sound=True, message="The Timer has Completed!\nSeconds elapsed:"+str(elapsed)+"s.")
-        notify = False
+        if (timeleft.total_seconds() < 1) and notify:
+            app.title = "Done!"
+            rumps.notification(title="Countdown Done!", subtitle="TimerBar", sound=True, message="The Timer has Completed!")
+            notify = False
+            target_time = None
 
-def startcount(mins):
+def startcount(target):
     """start a counter for num mins"""
-    global timeleft
+    global target_time
     global notify
-    global start_time
-    timeleft = mins * 60 #convert to seconds
-    start_time = time.time()
 
+    print 'target: %s' % target
+
+
+    target_time = target
     notify = True
-    rumps.notification(title="Countdown has begun.", subtitle="TimerBar", message="Duration: "+str(mins)+"min.\nTime Left: "+str(timeleft)+"s.\nA Notification will be sent when the timer stops.")
+
+    rumps.notification(title="Countdown has begun.", subtitle="TimerBar", message="Ends at %s"%target)
 
 @rumps.clicked("Start Timer", "5:00")
 def fivemincall(sender):
     """ start a counter for five mins"""
-    startcount(5)
+    target = datetime.now() + timedelta(minutes=5)
+    startcount(target)
 
 @rumps.clicked("Start Timer", "10:00")
 def tenmincall(sender):
     """ start a counter for ten mins"""
-    startcount(10)
+    target = datetime.now() + timedelta(minutes=10)
+    startcount(target)
 
 @rumps.clicked("Start Timer", "15:00")
 def fifteenmincall(sender):
     """ start a counter for fifteen mins"""
-    startcount(15)
+    target = datetime.now() + timedelta(minutes=15)
+    startcount(target)
 
 @rumps.clicked("Start Timer", "25:00")
 def fifteenmincall(sender):
     """ start a counter for twenty-five mins"""
-    startcount(25)
+    target = datetime.now() + timedelta(minutes=25)
+    startcount(target)
 
 @rumps.clicked("Start Timer", "Custom...")
 def customcall(sender):
-    response = rumps.Window("Enter number of minutes").run()
+    response = rumps.Window("Enters number of minutes").run()
+    p = parsedatetime.Calendar(parsedatetime.Constants())
+
+    print response
     if response.clicked:
         try:
-            startcount(float (response.text))
+            print(datetime(*p.parse(response.text)[0][:6]))
+            startcount(datetime(*p.parse(response.text)[0][:6]))
         except Exception, e:
-            pass
+            print e
 
 @rumps.clicked("Stop")
 def stoptimer(sender):
-    global timeleft
+    global target_time
     """Set the time left to zero"""
-    timeleft = 0
+    target_time = datetime.now()
 
 @rumps.clicked("About TimerBar")
 def aboutButton(sender):
-    rumps.Window(title="TimerBar, by Alexander O'Connor", message="Thanks for using TimerBar! Please feel free to send feedback on twitter to @uberalex", default_text="To use it, just run the app and select the time limit you would like to count down to. You can click the stop button any time.\n\nThe Custom... range will accept decimal values (so 0.5 will give a 30 second countdown).\n\nNote that it's not second-accurate.").run()
+    rumps.Window(title="TimerBar, by Alexander O'Connor", message="Thanks for using TimerBar! Please feel free to send feedback on twitter to @uberalex", default_text="To use it, just run the app and select the time limit you would like to count down to. You can click the stop button any time.\n\nThe Custom... box will take a variety of input such as '5 minutes' or 'at 4pm' or 'tomorrow' (see https://code.google.com/p/parsedatetime/ for more examples).").run()
 
 #App Definition
-app = rumps.App("Timebar", title="(00:00)", icon="data/rooster-128.png")
+app = rumps.App("Timebar", title="(00:00:00)", icon="data/rooster-128.png")
 
 #The Menu
 app.menu = [
@@ -108,7 +115,6 @@ app.menu = [
         None,
         rumps.MenuItem("Stop")
 ]
-
 
 
 
